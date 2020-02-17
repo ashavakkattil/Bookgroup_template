b29992bbde927e39b8330ba5ef8b4c7797dd8788
Asha <asha.vakkattil@gmail.com>
2020-02-17 17:46:37 +0530

    Added bookgroup logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-app-bar color="#f5f5f5" dense flat>
-      <v-row class="top-panel ma-auto" >
+      <v-row class="top-panel ma-auto">
         <v-col class="social-group" sm="1">
           <v-icon size="20">mdi-facebook</v-icon>
           <v-icon class="ml-4" size="20">mdi-twitter</v-icon>
@@ -18,10 +18,20 @@
           </v-row>
         </v-col>
         <v-col class="search-bar" sm="1">
-          <v-text-field class="search-text-bar" outlined dense single-line="" placeholder="Search..." append-icon="mdi-magnify"></v-text-field>
+          <v-text-field
+            class="search-text-bar"
+            outlined
+            dense
+            single-line
+            placeholder="Search..."
+            append-icon="mdi-magnify"
+          ></v-text-field>
         </v-col>
       </v-row>
     </v-app-bar>
+    <div class="logo-container">
+      <v-img src="../assets/logo.png" />
+    </div>
   </div>
 </template>
 <script>
@@ -40,16 +50,21 @@ export default {
   max-width: 1730px;
   height: inherit;
 }
-.top-panel .col{
-    height: inherit;
+.top-panel .col {
+  height: inherit;
 }
-.search-text-bar .v-input__control .v-input__slot{
-    min-height: 20px !important
+.search-text-bar .v-input__control .v-input__slot {
+  min-height: 20px !important;
 }
 .top-panel-message {
   font-size: 14px;
 }
 .top-panel-message .col {
   padding: 0px;
+}
+.logo-container {
+  padding: 54px 6rem 11px;
+  width: 480px;
+  margin: auto;
 }
 </style>
