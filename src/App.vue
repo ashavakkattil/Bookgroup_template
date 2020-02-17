e1e039c2a1c3aa93aa7cc32c11d63cab220677be
Asha <asha.vakkattil@gmail.com>
2020-02-17 16:33:17 +0530

    App-bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <Appheader />
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Appheader from "./components/AppHeader";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Appheader
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lato');
+#app {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.625;
+  font-family: Lato, sans-serif;
+  letter-spacing: 0.04em;
+  text-align: left;
+  color: #323232;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-app-bar color="#f5f5f5" dense flat>
+      <v-row class="top-panel ma-auto" >
+        <v-col class="social-group" sm="1">
+          <v-icon size="20">mdi-facebook</v-icon>
+          <v-icon class="ml-4" size="20">mdi-twitter</v-icon>
+          <v-icon class="ml-4" size="20">mdi-google-plus</v-icon>
+        </v-col>
+        <v-col class="top-panel-message" sm="10">
+          <v-row>
+            <v-col sm="7">Powered by Experts. Published by you.</v-col>
+            <v-col sm="2">
+              Call Us Today
+              <span class="font-weight-black">555.123.2323</span>
+            </v-col>
+            <v-col sm="3">22 St. Black Road Raleigh, PA 34578</v-col>
+          </v-row>
+        </v-col>
+        <v-col class="search-bar" sm="1">
+          <v-text-field class="search-text-bar" outlined dense single-line="" placeholder="Search..." append-icon="mdi-magnify"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isClicked: false,
+      showIcon: true
+    };
+  }
+};
+</script>
+<style >
+.top-panel {
+  margin: 0 auto;
+  max-width: 1730px;
+  height: inherit;
+}
+.top-panel .col{
+    height: inherit;
+}
+.search-text-bar .v-input__control .v-input__slot{
+    min-height: 20px !important
+}
+.top-panel-message {
+  font-size: 14px;
+}
+.top-panel-message .col {
+  padding: 0px;
+}
+</style>
